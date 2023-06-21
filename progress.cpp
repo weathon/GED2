169a3fa1ff0ac9612921c6bb1c2c0f7873c0c767
Wayne <wguo6358@gmail.com>
2023-06-21 22:29:09 +0000

    ETL added

--- progress.cpp
+++ progress.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <time.h>
 #include <unistd.h>
+
+bool firstTime = true;
+int startedTime;
+int ETL;
 void printProgressBar(int progress, int total, int barWidth = 40) {
   // GPT
+  if (firstTime) {
+    startedTime = time(0);
+    firstTime = false;
+  }
+  if (progress % 50 != 0 && progress != total)
+    return;
+
   float ratio = static_cast<float>(progress) / total;
   int filledWidth = static_cast<int>(ratio * barWidth);
-
-  std::cout <<" "<< progress << "/" << total<<" [";
+  ETL =  (time(0)-startedTime)/(progress+0.001)*(total-progress);
+  std::cout << " " << progress << "/" << total << " [";
   for (int i = 0; i < barWidth; ++i) {
     if (i < filledWidth)
       std::cout << "=";
@@ -14,7 +26,7 @@ void printProgressBar(int progress, int total, int barWidth = 40) {
     else
       std::cout << " ";
   }
-  std::cout << "] " << static_cast<int>(ratio * 100.0) << "%\r";
+  std::cout << "] " << static_cast<int>(ratio * 100.0) << "% "<< "Estimated Time Left: " << ETL/60 <<" min"<<ETL%60<< "sec \r";
   std::cout.flush();
 }
 
